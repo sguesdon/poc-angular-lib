6d65d12d70ef4b37e5c7dc40d89e2e44c6fd2f5d
Sebastian Guesdon <sebastian.guesdon@gmail.com>
2021-09-10 22:28:01 +0200

    init

---
+++ poc-angular-lib/projects/sguesdon/mylib/src/lib/mylib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-mylib',
+  template: `
+    <p>
+      mylib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MylibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
